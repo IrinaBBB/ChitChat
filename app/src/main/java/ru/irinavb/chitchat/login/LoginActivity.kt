a47ba163fcdea836a6ac3a94246c9cad0fb1105c
IrinaBBB <balkarovairina@gmail.com>
2022-05-03 09:08:53 +0200

    Add xml layout for Login

---
+++ app/src/main/java/ru/irinavb/chitchat/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package ru.irinavb.chitchat.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.irinavb.chitchat.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
